b7a0ddfa6edd1d5102df1bd0ad8a0f5b9b435bad
ChiaWen-Kao <chiawen.kao@uqconnect.edu.au>
2023-10-06 17:01:18 +1000

    Finish first version of search bar function and all-activity.html page
    + Finish filter layout
    + Search bar of index.html can pass parameter to all-activity.html
    + Finish function of fetch community activity data from API, and display the data based on user's condition search
    + Finish total result records

--- js/site_interaction.js
+++ js/site_interaction.js
@@ -1,4 +1,38 @@
 /* 
+    Home: Search bar
+    
+    - Before turn to all-activity.html, the search bar in index.html will collect user's search conditions.
+    - After collecting, it will list related activity in all-activity.html
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const searchSubmit = document.getElementById("searchbar-submit");    // button
+    
+    searchSubmit.addEventListener("click", function() {
+
+        // get condition value of search
+        var searchKeyword = document.getElementById("keyword-search").value;    // name, description...
+        var searchLocation = document.getElementById("location").value;    // location
+        var searchCategory = document.getElementById("advanced-search-category").value;    // event_type
+        var searchDateTime = document.getElementById("activity-date").value;    // date_time
+
+        // construct a string
+        var queryParams = {
+            keyword: searchKeyword,
+            location: searchLocation,
+            category: searchCategory,
+            dateTime:searchDateTime
+        }
+        const queryString = new URLSearchParams(queryParams).toString();
+        const urlWithParams = "all-activity.html"+"?"+queryString;
+
+        // pass parameter via url
+        document.location.href = urlWithParams;
+    });
+});
+
+/* 
     Comment 
 */
 const commentBaseURL = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/comments/";
@@ -52,24 +86,165 @@ fetch(getCommentsURL, {    // Fetch comment API
         });
     });
 
+
+/*
+    Get Activity
+*/
+$(document).ready(function () {
+    // pagination
+    const cardPerPage = 4;
+    const popularActivityCardContainer = document.getElementById("popular-activity-card");    // create card in this container
+    const prevButton = document.getElementById("popular-activity-prev");    // previous button
+    const nextButton = document.getElementById("popular-activity-next");    // next button
+    let currentPage = 1;
+
+    // fetch activity data from api
+    const activityBaseURL = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community_events/";
+    
+    const my_website_code = 'pete123'
+    const queryParams = {
+        website_code: my_website_code,
+    }
+    
+    const queryString = new URLSearchParams(queryParams).toString();
+    const urlWithParams = activityBaseURL+"?"+queryString;
+    
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch(urlWithParams, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+
+        console.log(data)
+
+        // display cards for specific page
+        function displayPage(page) {
+            popularActivityCardContainer.innerHTML = "";    // clear cards in container
+            const startIndex = (page - 1) * cardPerPage;
+            const endIndex = startIndex + cardPerPage;
+
+            for (let i = startIndex; i < endIndex; i++) {
+                const cardData = data[i];
+                const popularActivityCard = document.createElement("div");
+                popularActivityCard.classList.add("card", "border-light", "p-1", "m-1");
+                popularActivityCard.style.width = "20rem";
+
+                popularActivityCard.innerHTML = `
+                    <button type="button" class="wishlist" aria-label="add-wishlist"></button>
+                    <span class="badge text-bg-warning text-center sale-label">Sale</span>
+                    <img src="${cardData.photo}" class="img-fluid rounded card-img-top" alt="natural-meditation" />
+                    <div class="card-body pb-2">
+                        <h5 class="card-title">${cardData.name}</h5>
+                        <div class="row mb-2">
+                            <div class="col-md-2">
+                            <img width="100%" src="https://img.icons8.com/ios/50/959595/navigation.png" alt="location"/>
+                            </div>
+                            <div class="col-sm-10 p-0">
+                            <span class="text-muted fw-light">${cardData.location}</span>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-2">
+                            <img width="100%" src="https://img.icons8.com/ios/50/959595/clock--v3.png" alt="time" />
+                            </div>
+                            <div class="col-md-10 p-0">
+                            <span class="text-muted fw-light">${cardData.date_time}</span>
+                            </div>
+                        </div>
+                        <a href="#" class="btn btn-primary" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">${cardData.event_type}</a>
+                        <div class="row align-items-center mt-4">
+                            <div class="col-md-2 pe-0">
+                            <img width="70%" src="https://img.icons8.com/ios-filled/50/FFC500/star--v1.png" alt="star--v1"/>
+                            </div>
+                            <div class="col-md-4 ps-0">
+                            <span>4.8</span>
+                            <span>(100)</span>
+                            </div>
+                            <div class="col-md-6 text-end fw-bold fs-5" style="color: var(--orange);">AUD $85</div>
+                        </div>
+                    </div>
+                `;
+
+                // Create card in the container
+                popularActivityCardContainer.appendChild(popularActivityCard);
+            }
+        }
+
+        // initialize pagination
+        function initPagination() {
+            const totalCards = data.length;
+            const totalPages = Math.ceil(totalCards / cardPerPage);
+
+            // disable or enable the previous and next buttons based on the current page
+            function updateButtons() {
+
+                if (currentPage == 1) {
+                    prevButton.setAttribute("disabled", true);
+                } else if (currentPage == totalPages) {
+                    nextButton.setAttribute("disabled", true);
+                } else {
+                    prevButton.removeAttribute("disabled");
+                    nextButton.removeAttribute("disabled");
+                }
+            }
+
+            prevButton.addEventListener("click", function () {
+                if (currentPage > 1){
+                    currentPage --;
+                    displayPage(currentPage);
+                    updateButtons();
+                }
+            });
+            
+            nextButton.addEventListener("click", function () {
+                if (currentPage < totalPages) {
+                    currentPage++;
+                    displayPage(currentPage);
+                    updateButtons();
+                }
+            });
+
+            displayPage(currentPage);
+            updateButtons();
+        }
+
+        initPagination();
+    });
+});
+
 /* 
     Current Location (current location and nearby activity marks)
 */
-// navigator.geolocation.getCurrentPosition(position => {
-//     const { latitude, longitude } = position.coords;
-//     console.log(latitude, longitude)
+navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude)
     
-//     // Show a map centered at latitude / longitude.
-//     const mapContainer = document.getElementById("map");    // create map in this container
+    // Show a map centered at latitude / longitude.
+    const mapContainer = document.getElementById("map");    // create map in this container
 
-//     mapContainer.innerHTML = `
-//         <iframe width="100%" height="auto" style="border:0" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCIPd3N7noO3Mv0KFGY40KC2pvWX04oXjE&q=${latitude},${longitude}"></iframe>
-//     `;
-// });
+    mapContainer.innerHTML = `
+        <iframe width="100%" height="auto" style="border:0" allowfullscreen referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCIPd3N7noO3Mv0KFGY40KC2pvWX04oXjE&q=${latitude},${longitude}"></iframe>
+    `;
+});
+
 
 /*
-    Datepicker
+    all activity: total result records
 */
-$(function(){
-    $('#datepicker').datepicker();
-  });
+var resultRecord = 0;
+
+
+/*
+    all activity: sorted by
+*/
+// $(document).ready(function() {
+//     const sorteddropdownMenu = document.getElementById("dropdown-menu-sorted");    // get the action value from this element
+//     sorteddropdownMenu.addEventListener("click", function() {
+        
+//     })
+// });
+// var value = sorteddropdownMenu.value;
+// console.log(value)
